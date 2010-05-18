10db6e44bd51b76d3bda3bc540ce29eee962ec2f
Anna Shipunova <tetya-loshad@yandex.ru>
2010-05-18 14:40:01 -0400

    intermediate, upload files

--- sinatra/reconcile.rb
+++ sinatra/reconcile.rb
@@ -20,8 +20,18 @@ get '/' do
 end
 
 post '/submit' do
+  
   puts "=" * 80
   # puts params.inspect.to_s
+# "upload1"=>{:type=>"text/plain", :tempfile=>#<File:/var/folders/wq/wqTzo7SOHx4V9VsmKFOn5k+++TM/-Tmp-/RackMultipart20100518-8810-1l0kb7l-0>, :head=>"Content-Disposition: form-data; name=\"upload1\"; filename=\"text_bad.txt\"\r\nContent-Type: text/plain\r\n", :name=>"upload1", :filename=>"text_bad.txt"},
+
+  # file_field = @params['form']['file'] rescue nil
+  # file_field is a StringIO object
+  # file_field = params["upload1"][:tempfile] rescue nil
+  # puts file_field.path # 'text/csv'
+  # puts file_field.full_original_filename
+  # get_file(params['upload1'])
+  # get_file(params['upload2'])
   # {"url1"=>"http://localhost/text_bad.txt", "url2"=>"", "url_e"=>"Nlist2.txt", "freetext1"=>"", "freetext2"=>"", "upload1"=>"", "upload2"=>"", "func"=>"submit"}
   # key = url1, value = http://localhost/text_bad.txt
   # key = url2, value = 
@@ -31,12 +41,54 @@ post '/submit' do
   # key = upload1, value = 
   # key = upload2, value = 
   # key = func, value = submit
-  
-  params.each do |key, value|
-    unless value.empty?
-      instance_variable_set("@#{key}", value)
-    end
+  # unless (params['upload1'].empty?)
+    # save(params['upload1'])
+  # end
+  unless (params['upload1'].empty?)
+    time_tmp = Time.now.to_f.to_s   
+    filename = File.join("tmp/", time_tmp+params[:upload1][:filename])
+    f = File.open(filename, 'wb') 
+      f.write(params['upload1'][:tempfile].read)
+    # end
+    f.close
+    @url1 = "http://localhost/sinatra/"+filename
+    puts @url1
   end
+  unless (params['upload2'].empty?)
+    time_tmp = Time.now.to_f.to_s   
+    filename = File.join("tmp/", time_tmp+params[:upload2][:filename])
+    f = File.open(filename, 'wb') 
+      f.write(params['upload2'][:tempfile].read)
+    # end
+    f.close
+    @url2 = "http://localhost/sinatra/"+filename
+    puts @url2
+  end
+  # unless (params['upload2'].empty?)
+  #   time_tmp = Time.now.to_f.to_s   
+  #   filename = File.join("tmp/", time_tmp+params[:upload2][:filename])
+  #   f = File.open(filename, 'wb') do |file|
+  #     file.write(params['upload1'][:tempfile].read)
+  #   end
+  #   f.close
+  #   @url2 = filename
+  # end
+  # params.each do |key, value|
+  #   unless (value.nil?) #value.empty? || 
+  #     instance_variable_set("@#{key}", value)
+  #   end
+  #   unless (params['upload1'].empty?)
+  #     key = "url1"
+  #     value = params['upload1'][:tempfile].path
+  #     instance_variable_set("@#{key}", value)
+  #   end
+  #   unless (params['upload2'].empty?)
+  #     key = "url2"
+  #     value = params['upload2'][:tempfile].path
+  #     instance_variable_set("@#{key}", value)
+  #   end
+  #   print "@url1 = %s, value = %s\t@url2 = %s\n" % [@url1, value, @url2]
+  # end
   # if (params["url1"] && params["url2"])
   #   content = "url";
   #   url1 = params["url1"]
@@ -45,6 +97,9 @@ post '/submit' do
   if (@url1 && @url2)
     result = RestClient.get "http://localhost:4567/match?url1=#{@url1}&url2=#{@url2}"
   end
+  # if (@upload1 && @upload2)
+  #   result = RestClient.get "http://localhost:4567/match?url1=#{@upload1}&url2=#{@upload2}"
+  # end
 
 
   possible_names = result.split("\n");
@@ -68,6 +123,14 @@ def build_master_lists
     mfile_names << File.basename(mfile_name)
   end
   return mfile_names
+end
+
+def get_file(picture_field)
+  # {"url1"=>"", "url2"=>"", "url_e"=>"none", "freetext1"=>"", "freetext2"=>"", "upload1"=>{:type=>"text/plain", :tempfile=>#<File:/var/folders/wq/wqTzo7SOHx4V9VsmKFOn5k+++TM/-Tmp-/RackMultipart20100518-8810-1l0kb7l-0>, :head=>"Content-Disposition: form-data; name=\"upload1\"; filename=\"text_bad.txt\"\r\nContent-Type: text/plain\r\n", :name=>"upload1", :filename=>"text_bad.txt"}, "upload2"=>{:type=>"text/plain", :tempfile=>#<File:/var/folders/wq/wqTzo7SOHx4V9VsmKFOn5k+++TM/-Tmp-/RackMultipart20100518-8810-92cm9n-0>, :head=>"Content-Disposition: form-data; name=\"upload2\"; filename=\"text_good.txt\"\r\nContent-Type: text/plain\r\n", :name=>"upload2", :filename=>"text_good.txt"}}
+  
+  @name = picture_field.respond_to?(:original_filename) ? base_part_of(picture_field.original_filename) : nil
+  @content_type = picture_field.respond_to?(:content_type) ? picture_field.content_type.chomp : nil
+  @data = picture_field.respond_to?(:read) ? picture_field.read : picture_field.to_s
 end
 
 # # My 'helpers'
